e6d17c5ac062d6bc9c9d9a289bdfb4598d9f2ba4
Nibirhossainnir <144318970+Nibirhossainnir@users.noreply.github.com>
2023-11-22 16:18:38 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+var input = document.querySelector("#input");
+var form = document.querySelector("form");
+var body = document.querySelector("body");
+var button = document.querySelector("button");
+window.onload = function(){
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+ }
+ else{
+   alert("Geolocation is not supported by this browser.");
+ }
+};
+
+
+const showPosition = (position) => {
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
+    input.value = "Latitude:"+lat +", "+ "Longitude:"+long;
+    if(input.value == ""){
+      alert("Something wrong! please reload the page.")
+    }else{
+      form.submit();
+    }
+    };
+
+    const showError = (error) =>{
+      switch (error.code){
+        case error.PERMISSION_DENIED:
+          alert("Access to your device location is required.");
+          break;
+
+          case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable.");
+            break;
+
+            case error.TIMEOUT:
+              alert("The request to get user location timed out.");
+              break;
+
+              case error.UNKNOWN_ERROR:
+                alert("An unknown error occurred.");
+                break;
+
+                default:
+                  alert("An unknown error occurred.");
+      }
+      
+    };
